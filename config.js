d6fa6052622a6d1e9deda7837664f217f5f5f973
Michael <xmykyx@gmail.com>
2022-12-15 17:26:09 -0800

    update

--- config.js
+++ config.js
@@ -2,14 +2,12 @@
 
 /** Shared config for application; can be required many places. */
 
-
-
 require("dotenv").config();
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = process.env.PORT || 3001;
+const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
